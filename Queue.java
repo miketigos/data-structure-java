e00b430146e3ce0b556d32e97db69ae63b25be77
michael thomas <michaelthomas@sandiego.edu>
2021-02-19 09:03:18 -0800

    updated queue

--- Queue.java
+++ Queue.java
@@ -1,52 +1,59 @@
 package dataStructuresJava;
 
-/**
- * Linked list implementation of a queue ADT
- * First = Front of queue; Last = Back of queue
- *
- */
+// Linked list implementation of a queue ADT
+// * First = Front of queue; Last = Back of queue
+
+import java.util.NoSuchElementException;
 
 public class Queue<T extends Comparable<T>> {
 
     private LinkedList<T> queue;
 
-    // constructors
+    //--constructors--//
+    // default - creates and empty queue
     public Queue() {
         queue = new LinkedList<>();
     }
 
+    // instantiate with an initial element
     public Queue(T data) {
         queue = new LinkedList<>(data);
     }
 
     public T peek() {
-        if (isEmpty()) {
-            throw new RuntimeException("Queue is Empty!");
+        if (!queue.isEmpty()) {
+            return queue.peekFirst();
+        }
+        return null;
+    }
+
+    public T element() {
+        if (queue.isEmpty()) {
+            throw new NoSuchElementException();
         }
         return queue.peekFirst();
     }
 
-    public int size() {
-        return queue.size();
-    }
-
-    public boolean isEmpty() {
-        return (queue.size() == 0);
-    }
-
-    public void offer(T data) {
+    public boolean offer(T data) {
         queue.addLast(data);
+        return true;
     }
 
     public T poll() {
-        if (isEmpty()) {
-            throw new RuntimeException("Queue is Empty!");
+        if (!queue.isEmpty()) {
+            return queue.removeFirst();
         }
-        return queue.removeLast();
+        return null;
+    }
+
+    public T remove() {
+        if (queue.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.removeFirst();
     }
 
     public String toString() {
         return queue.toString();
     }
-
 }
